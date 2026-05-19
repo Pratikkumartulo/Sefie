d539f7bdb5eccedfb1aeaa3f29715a9f63b26a03
pratikkumartulo <pratiktulo42@gmail.com>
2026-05-19 18:36:26 +0530

    Debug Option for Production

--- app.py
+++ app.py
@@ -411,4 +411,5 @@ def webhook():
 #     return jsonify(body)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    debug_mode = os.getenv("FLASK_DEBUG", "false").lower() == "true"
+    app.run(debug=debug_mode)
